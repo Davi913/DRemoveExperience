6408f346b3578c3fdbe333a3d0a90118164ea22e
Davi913 <rio.daviduarte@gmail.com>
2020-03-18 19:22:35 -0300

    :rocket: First Update 1.0-SNAPSHOT

---
+++ README.md
@@ -0,0 +1,11 @@
+# DRemoveExperience
+
+# Inglês
+
+This plugin was made to remove the experience that is dropped from such entities.
+It is possible to take advantage of its configuration (config.yml) to add or remove entities that will be affected by the plugin.
+
+# Português
+
+Esse plugin foi feito para remover a experiencia que é dropada de tais entidades. 
+É possível usufruir da sua configuração (config.yml) para adicionar ou remover entidades que serão afetadas pelo plugin.
---
+++ src/com/davizin/removeexperience/RemoveEXP.java
@@ -0,0 +1,54 @@
+package com.davizin.removeexperience;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveEXP extends JavaPlugin {
+
+    protected List<EntityType> removedEntityes = new ArrayList<>();
+
+    @Override
+    public void onEnable() {
+        loadMobs();
+        registerListeners();
+    }
+
+    private void registerListeners() {
+        Bukkit.getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            public void cancelExperience(EntityDeathEvent event) {
+                if (removedEntityes.contains(event.getEntity().getType())) event.setDroppedExp(0);
+            }
+        }, this);
+    }
+
+    private void loadMobs() {
+        if (!new File("config.yml").exists()) saveDefaultConfig();
+        if (!getConfig().contains("RemovedExperience")) getConfig().set("RemovedExperience", Arrays.asList("64", "99"));
+        getConfig().getStringList("RemovedExperience").forEach(line -> {
+            int id;
+            try {
+                id = Integer.parseInt(line);
+            }catch (Exception e) {
+                Bukkit.getConsoleSender().sendMessage("[DRemoveExperience] Não foi possível carregar o ID '" + line + "'");
+                return;
+            }
+            EntityType entity = EntityType.fromId(id);
+            if (entity == null) {
+                Bukkit.getConsoleSender().sendMessage("[DRemoveExperience] Não foi possível carregar o ID '" + line + "'");
+                return;
+            }
+            removedEntityes.add(entity);
+        });
+    }
+}
